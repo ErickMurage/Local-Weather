62c8a135dcb8c22716f93ed528582c997645a740
Murage <murageerick@gmail.com>
2018-05-26 18:53:06 +0300

    Getting geolocation data

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
+    
     $("#get-weather").on("click", function() {
         $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139", function (data) {
-
+            
+                      
             let weather = " ";
             let temperature = " ";
             let icon = " ";
             
             //weather += data["weather"][0]['main'];
-
+            
             //function weatherData (val) {
                 
                 Object.keys(data).forEach(function (item, index, array) {
+                    if (item == "coord") {
+                        console.log(data["coord"]["lon"]);
+                    }
 
                     if (item == "weather") {
                         data["weather"].forEach(function (item, index, array) {
@@ -25,8 +30,13 @@ $(document).ready(function () {
 
                 });
             //}
-            
-            
+            /*
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    $("#location-data").html("latitude: " + position.coords.latitude + " , longitude: " + position.coords.longitude);
+                });
+            }
+            */
             $("#weather").html(weather);
             $("#temperature").html(temperature);
             $("#location").html();
