21bb80e32553d43a1187cc5861922ea52a2a2400
Murage <murageerick@gmail.com>
2018-05-26 22:45:43 +0300

    Removed button (no button required to load data)

--- main.js
+++ main.js
@@ -1,47 +1,49 @@
 $(document).ready(function () {
     
-    $("#get-weather").on("click", function() {
-        $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139", function (data) {
-            
-                      
-            let weather = " ";
-            let temperature = " ";
-            let icon = " ";
-            
-            //weather += data["weather"][0]['main'];
-            
-            //function weatherData (val) {
+    //$("#get-weather").on("click", function() {
+        
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                let latitude = position.coords.latitude;
+                let longitude = position.coords.longitude;
                 
-                Object.keys(data).forEach(function (item, index, array) {
-                    if (item == "coord") {
-                        console.log(data["coord"]["lon"]);
-                    }
-
-                    if (item == "weather") {
-                        data["weather"].forEach(function (item, index, array) {
-                            weather += item["description"];
-                            icon += "<img src = '"+ item["icon"] +"' >"
-                        });     
-                    }
-                    if (item == "main") {
-                        temperature += data["main"]["temp"] + "⁰c";
-                    }
+                $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "&lon=" + longitude, function (data) {
+            
+                    let weather = " ";
+                    let temperature = " ";
+                    let icon = " ";
                     
+                    Object.keys(data).forEach(function (item, index, array) {
 
+                        if (item == "weather") {
+                            data["weather"].forEach(function (item, index, array) {
+                                weather += item["description"];
+                                icon += "<img src = '"+ item["icon"] +"' >"
+                            });     
+                        }
+                        if (item == "main") {
+                            temperature += data["main"]["temp"] + "⁰c";
+                        }
+                        
+                    });
+                
+                    console.log(temperature);
+
+                    function temperatureConverter () {
+                        
+                    }
+
+
+                    $("#weather").html(weather);
+                    $("#temperature").html(temperature);
+                    $("#location").html();
+                    $("#icon").html(icon);
+                    $("#switch").html();
                 });
-            //}
-            /*
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function (position) {
-                    $("#location-data").html("latitude: " + position.coords.latitude + " , longitude: " + position.coords.longitude);
-                });
-            }
-            */
-            $("#weather").html(weather);
-            $("#temperature").html(temperature);
-            $("#location").html();
-            $("#icon").html(icon);
-            $("#switch").html();
-        });
-    });
+            });
+        }
+        
+        
+        
+    //});
 });
